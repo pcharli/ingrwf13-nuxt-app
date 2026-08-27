fde5d764eba521a53e5736749e78fcb53cc8bdd3
pcharli <pcharli@test.be>
2026-08-27 14:34:11 +0200

    update insert motos

--- server/api/motos/index.post.js
+++ server/api/motos/index.post.js
@@ -1,24 +1,21 @@
 export default defineEventHandler(async (e) => {
     const body = await readBody(e)
-    const newMoto = {
-        marque: body.marque,
-        modele: body.modele,
-        couleur: body.couleur
-    }
+  
     try {
         const [result] = await db.query(
     'INSERT INTO motos (marque, modele, couleur) VALUES (?, ?, ?)',
     [body.marque, body.modele, body.couleur]
   )
 
-  return {
-    id: result.insertId,
-    title: body.title,
-    duration: body.duration
-  }
+    return {
+        id: result.insertId,
+        title: body.title,
+        duration: body.duration
+    }
     } catch(error) {
         throw createError({
             statusCode: 500,
-            statusMessage: `Erreur MySQL: ${error.message}`        })
+            statusMessage: `Erreur MySQL: ${error.message}`
+        })
     }
 })
