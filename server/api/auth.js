59b212a2dcf2583aaa7b783732a12f2a02bf019e
pcharli <pcharli@test.be>
2026-08-27 15:50:55 +0200

    add token in response

--- server/api/auth.js
+++ server/api/auth.js
@@ -24,7 +24,7 @@ export default defineEventHandler(async (e) => {
                 httpOnly: true,           // Sécurité : empêche l'accès via document.cookie en JS client
                 path: '/'
             })
-            return { success: true, message: 'Connexion réussie' }
+            return { success: true, message: 'Connexion réussie', token: tokenValue }
         }
         else {
            throw createError({
